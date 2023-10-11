fb1c12e9ebcbb308dbf1049cc2061c1d5fe08f88
Beyza Kütük <beyzakutuk55@gmail.com>
2023-10-11 16:23:33 +0300

    Add Building.cs & BuildingPreset.cs

---
+++ Hafta12/Assets/Scripts/BuildingPreset.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName ="Building Preset" , menuName ="New Building Preset")]
+public class BuildingPreset : ScriptableObject
+{
+    public int cost;
+    public int costPerTurn;  //maliyet
+    public GameObject prefab;
+
+    public int population;
+    public int jobs;
+    public int food;
+}
